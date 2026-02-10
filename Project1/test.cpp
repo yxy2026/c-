d81972a9dcadd491fb954376a7d1d0c66f634b26
yxy2026 <2923486721@qq.com>
2026-02-10 09:00:29 +0800

    计算机基础

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int a = 2;
-	int b = 3;
-	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;
-	printf("a=%d,b=%d", a, b);
+	int a;
+	scanf_s("%d", &a);
+	switch (a / 10){
+	case 6:
+		printf("及格");
+		break;
+	case 7:
+	case 8:
+			printf("良好");
+			break;
+	case 9:
+				printf("优秀");
+	}
 	return 0;
 }
